6760d7b857bd32a296983389974b408bee8dbf9a
Simon Hundsdorfer <simon.hundsdorfer@tum.de>
2023-01-12 18:15:36 +0100

    Removed part of module path from nodes names

--- src/analysis/util.rs
+++ src/analysis/util.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use rustc_hir::def_id::{DefId, LOCAL_CRATE};
 use rustc_middle::ty::{List, TyCtxt};
 
@@ -6,17 +7,21 @@ pub fn def_path_debug_str_custom<'tcx>(tcx: TyCtxt<'tcx>, def_id: DefId) -> Stri
     let substs = List::identity_for_item(tcx, def_id);
 
     let crate_name = if def_id.is_local() {
-        tcx.crate_name(LOCAL_CRATE)
+        format!("{}::", tcx.crate_name(LOCAL_CRATE))
     } else {
         let cstore = tcx.cstore_untracked();
-        cstore.crate_name(def_id.krate)
+        format!("{}::!", cstore.crate_name(def_id.krate))
     };
 
-    let def_path_str = format!(
-        "{}::{}",
+    let mut def_path_str = format!(
+        "{}{}",
         crate_name,
         tcx.def_path_str_with_substs(def_id, substs)
     );
+
+    // This is a hack
+    let regex: Regex = Regex::new(r"!::([^:]*)").unwrap();
+    def_path_str = regex.replace_all(&def_path_str, "").to_string();
 
     def_path_str
 }
