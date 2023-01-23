226a5eb474a9ba94439ae731d0211485bf7c3e18
Simon Hundsdorfer <simon.hundsdorfer@tum.de>
2023-01-23 18:13:38 +0100

    Improved naming scheme

--- src/analysis/util.rs
+++ src/analysis/util.rs
@@ -42,11 +42,16 @@ pub fn def_path_debug_str_custom<'tcx>(tcx: TyCtxt<'tcx>, def_id: DefId) -> Stri
         tcx.def_path_str_with_substs(def_id, substs)
     );
 
+    let regex: Regex = Regex::new(r"(<.* as )(.*::)(.*>)").unwrap();
+    def_path_str = regex.replace_all(&def_path_str, "$1$3").to_string();
+
     // This is a hack
     // If this is a non-local def_id:
     //      We are removing the part of the path that corresponds to the alias name of the extern crate
     //      In this crate itself, this part of the path is not present
-    let regex: Regex = Regex::new(r"(![^:]*::)").unwrap();
+    let regex: Regex = Regex::new(r"(!)<").unwrap();
+    def_path_str = regex.replace_all(&def_path_str, "<").to_string();
+    let regex: Regex = Regex::new(r"(![^:]*?::)").unwrap();
     def_path_str = regex.replace_all(&def_path_str, "").to_string();
 
     def_path_str.replace("\n", "")
