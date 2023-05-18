51edfce31170e527ff6d9d1fbc517f82f27e631e
Simon Hundsdorfer <simon.hundsdorfer@tum.de>
2023-05-18 20:20:08 +0200

    Fix bug in naming system occurring only in names of non-local def_ids

--- src/names.rs
+++ src/names.rs
@@ -1,3 +1,5 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use rustc_hir::{
     def_id::{DefId, LOCAL_CRATE},
     definitions::DefPathData,
@@ -33,6 +35,26 @@ pub(crate) fn def_id_name<'tcx>(tcx: TyCtxt<'tcx>, def_id: DefId) -> String {
         def_path_str_with_substs_with_no_visible_path(tcx, def_id, substs)
     );
 
+    if !def_id.is_local() {
+        // In case this is a non-local def_id, the name of the crate is printed in generic types like <foo::Foo>
+        // We remove this crate prefix here, because it can lead to discontinuity in the dependency graph (static)
+        // and false traces (dynamic)
+
+        lazy_static! {
+            static ref RE_GENERICS_IMPL: Regex = Regex::new(r"<impl ([^>]*?)::").unwrap();
+            static ref RE_GENERICS_FOR: Regex = Regex::new(r"for ([^>]*?)::").unwrap();
+            static ref RE_GENERICS: Regex = Regex::new(r"<([^> ]*?)::").unwrap();
+        }
+
+        def_path_str = RE_GENERICS_IMPL
+            .replace_all(&def_path_str, "<impl ")
+            .to_string();
+        def_path_str = RE_GENERICS_FOR
+            .replace_all(&def_path_str, "for ")
+            .to_string();
+        def_path_str = RE_GENERICS.replace_all(&def_path_str, "<").to_string();
+    }
+
     // Occasionally, there is a newline which we do not want to keep
     def_path_str = def_path_str.replace("\n", "");
 
@@ -51,6 +73,12 @@ pub fn def_path_str_with_substs_with_no_visible_path<'t>(
     )
     .unwrap()
     .into_buffer()
+
+    //lazy_static! {
+    //    static ref RE_GENERICS: Regex = Regex::new(r"<.*>::").unwrap();
+    //}
+    //
+    //RE_GENERICS.replace_all(&result, "").to_string()
 }
 
 // Source: https://doc.rust-lang.org/stable/nightly-rustc/src/rustc_middle/ty/print/pretty.rs.html#1766
