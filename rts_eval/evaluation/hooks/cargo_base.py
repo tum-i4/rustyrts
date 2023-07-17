6776082ef65e33a579bdf100a09259c2eeb9e29c
Simon Hundsdorfer <simon.hundsdorfer@tum.de>
2023-07-17 12:17:00 +0200

    Remove submodule before updating

--- rts_eval/evaluation/hooks/cargo_base.py
+++ rts_eval/evaluation/hooks/cargo_base.py
@@ -92,7 +92,11 @@ class CargoHook(Hook, ABC):
             self.git_client.git_repo.git.reset(parent_commit, hard=True)
 
             for submodule in self.git_client.git_repo.submodules:
-                submodule.update(init=True, recursive=True, force=True, keep_going=True)
+                try:
+                    submodule.remove(module=True, force=True, configuration=True)
+                    submodule.update(init=True, recursive=True, force=True)
+                except:
+                    continue
 
             for filename in glob.glob("rust-toolchain*"):
                 os.remove(filename)
@@ -214,7 +218,11 @@ class CargoHook(Hook, ABC):
             self.git_client.git_repo.git.reset(commit.commit_str, hard=True)
 
             for submodule in self.git_client.git_repo.submodules:
-                submodule.update(init=True, recursive=True, force=True, keep_going=True)
+                try:
+                    submodule.remove(module=True, force=True, configuration=True)
+                    submodule.update(init=True, recursive=True, force=True)
+                except:
+                    continue
 
             for filename in glob.glob("rust-toolchain*"):
                 os.remove(filename)
