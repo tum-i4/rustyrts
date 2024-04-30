b9dacc04c9a74b46867f0eb32c65aeb80ed86b36
Simon Hundsdorfer <simon.hundsdorfer@tum.de>
2024-05-01 00:55:03 +0200

    Simplified test runner used by dynamic RustyRTS

--- rustyrts-dynamic-runner/src/lib.rs
+++ rustyrts-dynamic-runner/src/lib.rs
@@ -1,15 +1,27 @@
 #![feature(test)]
-#![feature(internal_output_capture)]
-
 extern crate test;
 
-mod constants;
-mod libtest;
-mod pipe;
-mod test_runner;
-mod util;
+use std::any::Any;
+use test::test_main_static_abort;
 
-pub use crate::test_runner::rustyrts_runner;
+#[no_mangle]
+pub fn rustyrts_runner(tests: &[&test::TestDescAndFn]) {
+    std::panic::set_hook(Box::new(|info| {
+        eprintln!("{}", info);
+        let payload = info
+            .payload()
+            .downcast_ref::<String>()
+            .map(|e| unsafe { std::mem::transmute(&**e) })
+            .or_else(|| info.payload().downcast_ref::<&'static str>().copied())
+            .unwrap_or("panic ocurred");
 
-#[cfg(unix)]
-mod fs_utils;
+        // This effectively bypasses the panic hook
+        std::panic::resume_unwind(Box::new(payload) as Box<dyn Any + Send>);
+    }));
+
+    // When panic=abort is set (which would normally lead to a call to test_main_static_abort), a separate process is
+    // forked for every test case.
+    // Additionally, a panic hook is set that aborts instead of unwinding. We can revoke this by resuming unwinding in
+    // an additional panic hook.
+    test_main_static_abort(tests);
+}
